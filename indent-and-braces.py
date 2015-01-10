c8b4afbbc157c961def645d71627e0f2e730243e
Epskampie <simon@frontwise>
2015-01-10 15:43:08 +0100

    Added seperate command from cursor

--- indent-and-braces.py
+++ indent-and-braces.py
@@ -1,6 +1,5 @@
 import sublime
 import sublime_plugin
-import copy
 
 def get_indentation_on_line(view, point): 
     pt = view.line(point).begin()
@@ -16,27 +15,36 @@ def get_indentation_on_line(view, point):
     return result
 
 class IndentAndBracesCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # self.view.insert(edit, 0, "Hello, World!")
-        # self.view.run_command('indent')
+    def run(self, edit, from_cursor = False):
         sel = self.view.sel()
-        # print(sel[0].a)
-        for r in copy.deepcopy(sel):
-            region = sublime.Region(self.view.line(r.begin()).begin(), self.view.line(r.end()).end())
-            # content = view.substr(region)
+        for r in tuple(reversed(sel)):
+            if from_cursor:
+                region = sublime.Region(r.begin(), self.view.line(r.end()).end())
+            else:
+                region = sublime.Region(self.view.line(r.begin()).begin(), self.view.line(r.end()).end())
+            
+            # sel.clear()
+            # sel.add(region)
             
             indent = get_indentation_on_line(self.view, region.begin())
-            
-            insert_start = indent + "{\n"
-            insert_end = "\n" + indent + '}'
+            if from_cursor:
+                insert_start = "{"
+            else:
+                insert_start = indent + "{\n"
+            insert_end = '\n' + indent + '}'
             
             self.view.insert(edit, region.begin(), insert_start)
             self.view.insert(edit, region.end() + len(insert_start), insert_end)
             
-            sel.clear()
-            sel.add(sublime.Region(region.begin() + len(insert_start), region.end() + len(insert_start)))
+            indent_region = sublime.Region(region.begin() + len(insert_start) + 1, region.end() + len(insert_start))
+            num_lines_indented = len(self.view.lines(indent_region))
+            
+            self.view.sel().clear()
+            self.view.sel().add(indent_region)
             self.view.run_command('indent')
             
+            # Move cursor to beyond last brace
             sel.clear()
-            pt = region.begin() + len(indent)
+            settings = self.view.settings()
+            pt = region.end() + len(insert_start) + len(insert_end) + num_lines_indented * (settings.get('tab_size') if settings.get('translate_tabs_to_spaces') else 1)
             sel.add(sublime.Region(pt, pt))
